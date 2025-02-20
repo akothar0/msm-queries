74c6c28fce4b456ed31600c025b5bb67b2306a07
Aditya Kothari <adityakothari99@gmail.com>
2025-02-20 22:25:20 +0000

    added youngest and eldest directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -13,4 +13,8 @@ class DirectorsController < ApplicationController
   def youngest
     render({ :template => "director_templates/youngest" })
   end
+
+  def eldest
+    render({ :template => "director_templates/eldest" })
+  end
 end
