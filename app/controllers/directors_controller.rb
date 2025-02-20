15358fde9b4182e706eb326d10beb4add7b1dacb
Aditya Kothari <adityakothari99@gmail.com>
2025-02-20 22:19:39 +0000

    added actors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -7,9 +7,10 @@ class DirectorsController < ApplicationController
     the_id = params.fetch("the_id")
     matching_records  = Director.where({ :id => the_id })
     @the_director = matching_records[0]
-
-    
-
     render({ :template => "director_templates/details" })
+  end
+
+  def youngest
+    render({ :template => "director_templates/youngest" })
   end
 end
