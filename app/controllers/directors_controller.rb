131299dbb34729cf9bfe2dbbc560eecf9519b16b
Aditya Kothari <adityakothari99@gmail.com>
2025-02-20 21:22:53 +0000

    show director details

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,6 +4,12 @@ class DirectorsController < ApplicationController
   end
 
   def show
+    the_id = params.fetch("the_id")
+    matching_records  = Director.where({ :id => the_id })
+    @the_director = matching_records[0]
+
+    
+
     render({ :template => "director_templates/details" })
   end
 end
